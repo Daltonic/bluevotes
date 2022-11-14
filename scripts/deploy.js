b053d5b4dd731ea2e9a5e8c5582d3dc413a80d68
Daltonic <darlingtongospel@gmail.com>
2022-11-14 11:18:24 +0100

    Deployment script setup

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const { ethers } = require('hardhat')
+const fs = require('fs')
+
+async function main() {
+  const Contract = await ethers.getContractFactory('BlueVotes')
+  const contract = await Contract.deploy()
+
+  await contract.deployed()
+
+  const address = JSON.stringify({ address: contract.address }, null, 4)
+  fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log('Deployed contract address', contract.address)
+  })
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
